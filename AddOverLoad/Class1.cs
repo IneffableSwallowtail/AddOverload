975d2d23bba66e9347c6861bd93f7b08563b1b90
A.J. Miller <ajmiller1206@gmail.com>
2022-09-16 15:46:54 -0400

    first commit

---
+++ AddOverLoad/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddOverload;
+
+public static class Class1
+{
+    public static int Add(int x, int y)
+    {
+        return x + y;
+    }
+    public static decimal Add(decimal x, decimal y)
+    {
+        return x + y;
+    }
+    public static string Add(decimal x, decimal y, bool dollars)
+    {
+        decimal sum = x + y;
+        if (dollars == true)
+        {
+            if (sum == 1) return $"{sum} dollar";
+            else return $"{sum} dollars";
+        }
+        else
+            return $"{sum}";
+    }
+
+    //methods for cases
+    public static void Case1()
+    {
+        Console.Write("\nEnter your first integer: ");
+        int firstInt = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter your second integer: ");
+        int secondInt = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine($"The sum of your integers is {Class1.Add(firstInt, secondInt)}.\n");
+    }
+    public static void Case2()
+    {
+        Console.Write("\nEnter your first decimal: ");
+        decimal firstDec = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Enter your second decimal: ");
+        decimal secondDec = Convert.ToDecimal(Console.ReadLine());
+        Console.WriteLine($"The sum of your decimals is {Class1.Add(firstDec, secondDec)}\n");
+    }
+
+    public static void Case3()
+    {
+        Console.Write("\nEnter your first decimal: ");
+        decimal firstMon = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Enter your second decimal: ");
+        decimal secondMon = Convert.ToDecimal(Console.ReadLine());
+        Console.Write("Are you adding money? Yes/no: ");
+        string money = Console.ReadLine().ToLower();
+        bool isMoney = (money == "yes") ? true : false;
+        Console.WriteLine($"The sum is {Class1.Add(firstMon, secondMon, isMoney)}.");
+    }
+}
---
+++ AddOverLoad/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+using AddOverload;
+
+while (true)
+{
+    bool cont = true;
+    
+    Console.WriteLine("\nSelect a function:\nSelect \"1\" for adding two integers" +
+                      "\nSelect \"2\" for adding two decimals\n" +
+                      "Select \"3\" for adding two decimals that may or may not be money");
+
+    while (cont == true)
+    {
+        string userInput = Console.ReadLine();
+        switch (userInput)
+        {
+            case "1":
+                Class1.Case1();
+                cont = false;
+                break;
+            case "2":
+                Class1.Case2();
+                cont = false;
+                break;
+            case "3":
+                Class1.Case3();
+                cont = false;
+                break;
+            default:
+                Console.WriteLine("Invalid selection");
+                cont = true;
+                break;
+        }
+    }
+    Console.Write("Do you want to continue? Yes/no: ");
+    if (Console.ReadLine().ToLower() == "yes") continue;
+    else break;
+}
